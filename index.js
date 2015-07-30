6ab25b06012aa6d6013c73a27e38af7b25813c94
Rob Waymouth <rob.waymouth@mcmurrytmg.com>
2015-07-30 16:00:55 -0400

    proof of concept parsing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 // Get a stylesheet
 // Return contents
 function getStyleSheet(file){
-
+    var data;
     return data;
 };
 
@@ -12,7 +12,7 @@ function getStyleSheet(file){
 // and push them into an array
 // Return the array
 function findVars(data){
-
+    var varsList;
     return varsList;
 }
 
@@ -21,9 +21,25 @@ function findVars(data){
 // Returns an object containing the selectors that
 // use the var
 
-function getVarUsage(var){
-
+function getVarUsage(varName){
+    var usage;
     return usage;
-} 
+}
 
+var readFile = function (filePath, onLoad) {
+    fs.readFile(filePath, {encoding: 'utf8'}, function (err, fileData) {
+        var matches = fileData.match(/[$].*:.+;/g);
+        var variables = matches.map(function(entry){
 
+            var temp = entry.split(':');
+            var pair = {
+                'var': temp[0],
+                'value': temp[1].replace(' ', '')
+            };
+            return pair;
+        });
+        onLoad(err, variables);
+    });
+};
+
+readFile('test.scss', console.log);
