f4daf333be511bc9613fee7c7a4889b0cfba41cf
Rob Waymouth <rob.waymouth@mcmurrytmg.com>
2015-07-30 10:28:57 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var _ = require('lodash');
+var fs = require('fs');
+
+// Get a stylesheet
+// Return contents
+function getStyleSheet(file){
+
+    return data;
+};
+
+// Search contents for variables
+// and push them into an array
+// Return the array
+function findVars(data){
+
+    return varsList;
+}
+
+// Find everywhere a Var is used in a collection
+// of stylesheets
+// Returns an object containing the selectors that
+// use the var
+
+function getVarUsage(var){
+
+    return usage;
+} 
+
+
